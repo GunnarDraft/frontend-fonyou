1a60633b1e98b3d4d3f794fdb3a3b9738c210392
gunnardraft <gunnardraft@live.com.mx>
2024-01-05 01:38:05 -0600

    test

--- webpack.config.js
+++ webpack.config.js
@@ -55,6 +55,7 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: 'assets/[name].css',
+      publicPath: '/frontend-fonyou/'
     }),
     new CleanWebpackPlugin(),
   ],
