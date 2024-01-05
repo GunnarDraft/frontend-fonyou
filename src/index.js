d6e699d81b36ee2f3afdc33d21d3602d84972700
gunnardraft <gunnardraft@live.com.mx>
2024-01-05 03:48:06 -0600

    test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from "react-dom/client"; 
+import ReactDOM from "react-dom/client";
 import App from './routes/App';
 import { Provider } from 'react-redux'
 import store from './store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
- 
+
 root.render(<Provider store={ store }>
-    <App />
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 </Provider>);
